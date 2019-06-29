6d81b9f35ed9e0bf4a1c941e78cfb2d3e06f016d
mohaned-mashaly <mohaned_boss@outlook.com>
2019-06-29 14:52:59 +0200

    Add Majors to Model

--- Udacity-courses/Backend-course/Wesenior/wesenior/main/models.py
+++ Udacity-courses/Backend-course/Wesenior/wesenior/main/models.py
@@ -6,24 +6,31 @@ from django.db import models
 class Majors(models.Model):
     Major_name = models.CharField(max_length =200)
     Major_Info = models.TextField()
-    Tutors_slug = models.CharField(max_length =200)
-    #Courses_slug = models.CharField(max_length=200)
+    Tutors_slug = models.CharField(max_length =200,default=1)
+    Courses_slug = models.CharField(max_length=200,default=1)
+    class Meta :
+        verbose_name_plural = "Majors"
+    def __str__(self):
+        return self.Major_name
 
 class Courses(models.Model):
     Course_name = models.CharField(max_length =200)
     Course_content = models.TextField()
-    #Reserve_slug= models.CharField(max_length =200)
-    #Recommend_slug = models.CharField(max_length =200)
-    #Majorname  = models.ForeignKey(Majors)
-
+    Reserve_slug= models.CharField(max_length =200)
+    Recommend_slug = models.CharField(max_length =200)
+    Majorname  = models.ForeignKey(Majors,verbose_name = "Majors")
+    class Meta :
+        verbose_name_plural = "Courses"
+    def __str__(self):
+        return self.Course_name
 class Tutor(models.Model):
     tutor_name  = models.CharField(max_length =200)
     tutor_age   = models.IntegerField()
     tutor_bio   = models.TextField()
-    course_name  = models.ForeignKey(Courses)
-
+    course_name  = models.ForeignKey(Courses,verbose_name = "Courses")
+    class Meta :
+        verbose_name_plural = "Tutors"
     def __str__(self):
-        return "Tutor-name :" + self.tutor_name + "\n" + "tutor-age:" + str(self.tutor_age)+ "\n" + "tutor-bio :" + self.tutor_bio + "\n" + "Major" + Major
-
+        return self.tutor_name
     #def __str__(self):
     #    return "Number of Available Tutors for " + self.Major_name + " is " + str(self.number_of_Tutors)

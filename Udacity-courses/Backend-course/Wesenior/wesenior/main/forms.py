e47afc31f7fcc0993cd2f32f1998922b61293015
mohaned-mashaly <mohaned_boss@outlook.com>
2019-07-31 14:27:04 +0200

    Update Become a Tutor Form

--- Udacity-courses/Backend-course/Wesenior/wesenior/main/forms.py
+++ Udacity-courses/Backend-course/Wesenior/wesenior/main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Tutor,Courses,Majors
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -14,20 +15,21 @@ class NewUserForm(UserCreationForm):
             user.save()
         return user
 class TutorForm(UserCreationForm):
-    Major=forms.CharField(label = 'Major name',required = True)
+
+    Major=forms.CharField(label = 'Major name(MGT,MET,Pharmacy and Bio Technology,Applied Arts)',required = True)
     age  = forms.IntegerField(label = 'Age',required = True)
     Bio  = forms.CharField(label = "Tell us more about you",required = True)
 
     class Meta :
         model = User
-        fields = ("username","password1","password2","Major","age","Bio")
-
+        fields = ("username","password1","password2","email","Major","age","Bio")
     def save(self,commit = True):
         user = super(TutorForm,self).save(commit = False)
         user.Major = self.cleaned_data['Major']
         user.age = self.cleaned_data['age']
         user.Bio = self.cleaned_data['Bio']
-
+        #T = Tutor(tutor_name = user.username,tutor_age = user.age,tutor_bio = user.Bio,course_name = Courses(None).save() )
+        #T.save()
         if commit :
             user.save()
         return user

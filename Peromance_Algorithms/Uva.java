644f02222d004a0df96a7f978bdf901c0285d32c
mohaned mashaly <30902228+12mohaned@users.noreply.github.com>
2020-02-16 14:38:11 +0200

    Update Uva.java

--- Peromance_Algorithms/Uva.java
+++ Peromance_Algorithms/Uva.java
@@ -9,7 +9,7 @@ public class Launch {
 	Set<String> Problems_Solved = new HashSet<String>();
 	WebDriver Safari_Driver = new SafariDriver(); 
 	Safari_Driver.get("https://uhunt.onlinejudge.org/api/subs-user/1019081");
-	String S = Safari_Driver.findElement(By.tagName("Problem ID")).getText();
+	String S = Safari_Driver.findElement(By.tagName("Body")).getText();
 	S = S.substring(S.indexOf('[')+1);
 	StringTokenizer st = new StringTokenizer(S," ");
 	

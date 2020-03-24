01e06d62bf40158baa53179310d1445fde5b5099
mohaned-mashaly <mohaned_boss@outlook.com>
2020-03-24 22:17:51 +0200

    html template

--- news-aggregator/template.go
+++ news-aggregator/template.go
@@ -1,32 +1,53 @@
 package main
-
 import (
 	"fmt"
 	"html/template"
+	"encoding/xml"
+	"io/ioutil"
 	"net/http"
+	"strings"
+	"time"
 )
 
 type NewsForm struct {
-	link string
+	Link [] string
+}
+
+type Sitemapindex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Locations []string `xml:"url>loc"`
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	Form := NewsForm{link: "https://www.washingtonpost.com/sitemaps/business.xml"}
-	fmt.Fprintf(w, "<h1>There is nothing here but a blank page go to the news </h1>")
+	fmt.Fprintf(w, "<h1>There is nothing here but a blank page go to the news</h1>")
 }
 
 func AggregateHandler(w http.ResponseWriter, r *http.Request) {
-	Form := NewsForm{link: "https://www.washingtonpost.com/sitemaps/business.xml"}
-	t, error := template.ParseFiles("basictemplating.html")
-	if error != nil {
-		fmt.Println(error)
-	}
-	t.Execute(w, Form)
+
+	var mainsitemap Sitemapindex
+	var news News
+	var Washignton_post string = "https://www.washingtonpost.com/sitemaps/index.xml"
+	resp, _ := http.Get(Washignton_post)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &mainsitemap)
+	resp.Body.Close()
+	resp, _ = http.Get(strings.TrimSpace(mainsitemap.Locations[0]))
+	bytes, _ = ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &news)
+	resp.Body.Close()			
+
+	Page := NewsForm{ Link: news.Locations }
+	t, _ := template.ParseFiles("basictemplating.html")
+	t.Execute(w, Page)
+
 }
 
 func main() {
-	http.HandleFunc("/Home", indexHandler)
+	http.HandleFunc("/Home",indexHandler)
 	http.HandleFunc("/news", AggregateHandler)
 	http.ListenAndServe(":8000", nil)
-	// Aggregate()
+ 
 }
